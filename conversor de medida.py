cf833503678f12116607eb17adc5ee28e05dfb1a
GABRIEL SENATORE COSTA <gabrielsenatore28@gmail.com>
2025-10-01 22:49:38 -0300

    Add files via upload

---
+++ conversor de medida.py
@@ -0,0 +1,11 @@
+m = float(input("Digite uma distancia em metros:"))
+
+km = m/1000
+hm = m/100
+dam = m/10
+
+dm = m*10
+cm = m*100
+mm= m*1000
+
+print(f"{km} km\n{hm} hm\n{dam} dam\n{dm} dm\n{cm} cm\n{mm} mm")
